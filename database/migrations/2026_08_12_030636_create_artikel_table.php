762829c8b6f89e54ef9177e4df7095639049aea9
Adi <adiwiranata2009@gmail.com>
2026-08-24 12:58:18 +0800

    memperbaiki migration

--- database/migrations/2026_08_12_030636_create_artikel_table.php
+++ database/migrations/2026_08_12_030636_create_artikel_table.php
@@ -6,25 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('artikels', function (Blueprint $table) {
             $table->id();
             $table->string('artikeltitle');
-            $table->mediumText('artikelcontent');
+            $table->longText('artikelcontent');
             $table->string('slug')->unique();
-            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
-            $table->timestamps();
+            $table->string('image')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
             $table->softDeletes();
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('artikels');
